f76383334376f9033ce44424fab0551afcd298f0
blind-contours <david_mccoy@berkeley.edu>
2022-11-03 16:14:58 -0700

    3d sim slurm run script

---
+++ sandbox/slurm_scripts/3D_CVtreeMLE_run1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=3Dr1CVtreeMLE
+#
+# Partition:
+#SBATCH --partition=savio3
+#
+
+#SBATCH --qos=biostat_savio3_normal
+#SBATCH --account=co_biostat
+
+# Number of nodes for use case:
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --exclusive
+#
+# Wall clock limit:
+#SBATCH --time 72:00:00
+#
+## Command(s) to run (example):
+module load r/4.0.3
+
+### for foreach+doSNOW ###
+R CMD BATCH --no-save ../03_run_3d_simulation_r1.R 2D_run1.Rout
